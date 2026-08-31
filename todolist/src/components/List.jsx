49081a8c14cab815f8d4a0d9352635c17cb8ebef
김영준 <dudwns1ms1234@gmail.com>
2026-08-31 16:53:52 +0900

    feat: 투두 검색 기능 구현

--- todolist/src/components/List.jsx
+++ todolist/src/components/List.jsx
@@ -1,10 +1,20 @@
 import TodoList from "./todoList";
+import { useState } from "react";
 const List = ({todos}) => {
+    const [query, setQuery] = useState('');
+    const onChangeHandle = (e) => {
+        setQuery(e.target.value);
+    }
+    const filterTodos = (q) => {
+        const keyword = q.trim().toLowerCase();
+        return todos.filter((todo) => todo.content.toLowerCase().includes(keyword));
+    }
+
     return(
         <div>
-            <input type="text" placeholder="검색할거"/>
+            <input value = {query} type="text" placeholder="검색할거" onChange={onChangeHandle}/>
             {
-                todos.map((todo)=>{
+                filterTodos(query).map((todo)=>{
                     return(<TodoList key={todo.id} todo = {todo}/>);
                 })
             }
